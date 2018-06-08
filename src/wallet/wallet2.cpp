eb3798ed93ef10dbaf8066c749ac16169b8b6dad
Interchained <president@worldvaporexpo.com>
2018-06-08 00:05:58 -0400

    add CRYPTONOTE_RINGDB_DIR to config

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -121,9 +121,9 @@ namespace
   std::string get_default_ringdb_path()
   {
     boost::filesystem::path dir = tools::get_default_data_dir();
-    // remove .bitmonero, replace with .shared-ringdb
+    // remove .shared-ringdb, replace with .shared-electronero-ringdb
     dir = dir.remove_filename();
-    dir /= ".shared-electronero-ringdb";
+    dir /= CRYPTONOTE_RINGDB_DIR; // move to config
     return dir.string();
   }
 }
